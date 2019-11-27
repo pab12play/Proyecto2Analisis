d787e1919c95eb04daccf63c7e7d2030c5019b75
J4SSGG <jasg.sotogutierrez@gmail.com>
2019-11-26 20:50:29 -0600

    Migración proyecto R a diagrama de archivos de repositorio

--- KMeans/SandBox.R
+++ KMeans/SandBox.R
@@ -1,49 +1,51 @@
-install.packages("readxl")
+#install.packages("readxl")
 library("readxl")
 
-install.packages("factoextra")
+#install.packages("factoextra")
 library("factoextra")
 
-install.packages("gridExtra")
+#install.packages("gridExtra")
 library("gridExtra")
 
+setwd("C:/Users/Abraham/Desktop/Proyecto2Analisis") #comentar y agregar sus rutas
+
 # xlsx files
-my_data <- read_excel("D:/Documents/Universidad/Octavo ciclo/ANÃLISIS DE DATOS/Proyecto2/ListadoPromedios.xlsx")
+my_data <- read_excel("ProjectData/ListadoPromedios.xlsx")
 set.seed(23544727)
 
 #Promedio simple x numero de cursos 
 PromedioSimple = my_data[,c("prom_simp_x_ciclo", "cursos_acumulados")]
 row.names(PromedioSimple) = my_data$ID
-nclustersPromSimp = fviz_nbclust(PromedioSimple, kmeans, method = "wss") + geom_vline(xintercept = 3 ,linetype = 2) + ggtitle("NÃºmero Ã³ptimo de clusters")
+nclustersPromSimp = fviz_nbclust(PromedioSimple, kmeans, method = "wss") + geom_vline(xintercept = 3 ,linetype = 2) + ggtitle("Número óptimo de clusters")
 clustersPromSimp = kmeans(PromedioSimple, 3, nstart = 20)
 graficakPromSimp = fviz_cluster(clustersPromSimp, data = PromedioSimple) + ggtitle("Promedio Simple x Numero de cursos")
-save(PromedioSimple, file = "Data/PromedioSimple.RData")
-pdf("Plots/PromedioSimple.pdf", width=30, height=50)
+save(PromedioSimple, file = "KMeans/Data/PromedioSimple.RData")
+pdf("KMeans/Plots/PromedioSimple.pdf", width=30, height=50)
 grid.arrange(graficakPromSimp, nclustersPromSimp, nrow = 2)
 dev.off()
 
 
 PromedioPonderado = my_data[, c("Promedio_Ponderado_Acumulado", "cursos_acumulados")]
 row.names(PromedioPonderado) = my_data$ID
-nclustersPromPond = fviz_nbclust(PromedioPonderado, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("NÃºmero Ã³ptimo de clusters")
+nclustersPromPond = fviz_nbclust(PromedioPonderado, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("Número óptimo de clusters")
 clustersPromPond = kmeans(PromedioPonderado, 3, nstart = 30)
-graficakPromPond = fviz_cluster(clustersPromPond, data = PromedioPonderado) + ggtitle("Promedio acomulado x NÃºmeroo de cursos")
-pdf("Plots/PromedioPond.pdf", width=30, height=50)
+graficakPromPond = fviz_cluster(clustersPromPond, data = PromedioPonderado) + ggtitle("Promedio acomulado x Númeroo de cursos")
+pdf("KMeans/Plots/PromedioPond.pdf", width=30, height=50)
 grid.arrange(graficakPromPond, nclustersPromPond, nrow = 2)
 dev.off()
 
-PromedioAno = my_data[, c("Ani", "prom_simp_x_ciclo")]
-PromedioAno$Ani = as.numeric(as.character(PromedioAno$Ani))
+PromedioAno = my_data[, c("anio", "prom_simp_x_ciclo")]
+PromedioAno$anio = as.numeric(as.character(PromedioAno$anio))
 row.names(PromedioAno) = my_data$ID
-nClustersPromAno = fviz_nbclust(PromedioAno, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("NÃºmero Ã³ptimo de clusters")
+nClustersPromAno = fviz_nbclust(PromedioAno, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("Número óptimo de clusters")
 clustersPromAno = kmeans(PromedioAno, 3, nstart = 30)
-graficakPromAno = fviz_cluster(clustersPromAno, data = PromedioAno) + ggtitle("Promedio acomulado x Ani (No relevante)")
-pdf("Plots/PromedioAni.pdf", width=30, height=50)
+graficakPromAno = fviz_cluster(clustersPromAno, data = PromedioAno) + ggtitle("Promedio acomulado x Año (No relevante)")
+pdf("KMeans/Plots/PromedioAni.pdf", width=30, height=50)
 grid.arrange(graficakPromAno, nClustersPromAno, nrow = 2)
 dev.off()
 
 # Estudiante 119
-my_data <- read.csv("D:/Documents/Universidad/Octavo ciclo/ANÃLISIS DE DATOS/Proyecto2/Notas/119.csv")
+my_data <- read.csv("ProjectData/DataSets/119.csv")
 NotasNumericas = my_data[]
 
 
