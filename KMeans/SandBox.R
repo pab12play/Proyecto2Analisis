368c0b871273a3bc8485b2bc442ba8cd9eb0712a
J4SSGG <jasg.sotogutierrez@gmail.com>
2019-11-26 21:59:51 -0600

    Cambios graficas

--- KMeans/SandBox.R
+++ KMeans/SandBox.R
@@ -6,12 +6,24 @@ library("factoextra")
 
 #install.packages("gridExtra")
 library("gridExtra")
+#install.packages("ggplot2")
+library(ggplot2)
 
 setwd("C:/Users/Abraham/Desktop/Proyecto2Analisis") #comentar y agregar sus rutas
 
 # xlsx files
 my_data <- read_excel("ProjectData/ListadoPromedios.xlsx")
 set.seed(23544727)
+
+#Plot de promedio simple por anio
+ggplot() + geom_point(aes(x = my_data$año, y =  (my_data$prom_simp_x_ciclo)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+
+#Plot de promedio ponderado por anio
+ggplot() + geom_point(aes(x = my_data$año, y =  (my_data$prom_pond_x_ciclo)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+
+#Plot de promedio ponderado por anio
+ggplot() + geom_point(aes(x = my_data$año, y =  (my_data$prom_simp_x_ciclo)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+
 
 #Promedio simple x numero de cursos 
 PromedioSimple = my_data[,c("prom_simp_x_ciclo", "cursos_acumulados")]
@@ -27,15 +39,15 @@ dev.off()
 
 PromedioPonderado = my_data[, c("Promedio_Ponderado_Acumulado", "cursos_acumulados")]
 row.names(PromedioPonderado) = my_data$ID
-nclustersPromPond = fviz_nbclust(PromedioPonderado, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("Número óptimo de clusters")
-clustersPromPond = kmeans(PromedioPonderado, 3, nstart = 30)
+nclustersPromPond = fviz_nbclust(PromedioPonderado, kmeans, method = "wss") + geom_vline(xintercept = 4, linetype = 2) + ggtitle("Número óptimo de clusters")
+clustersPromPond = kmeans(PromedioPonderado, 4, nstart = 100)
 graficakPromPond = fviz_cluster(clustersPromPond, data = PromedioPonderado) + ggtitle("Promedio acomulado x Númeroo de cursos")
 pdf("KMeans/Plots/PromedioPond.pdf", width=30, height=50)
 grid.arrange(graficakPromPond, nclustersPromPond, nrow = 2)
 dev.off()
 
-PromedioAno = my_data[, c("anio", "prom_simp_x_ciclo")]
-PromedioAno$anio = as.numeric(as.character(PromedioAno$anio))
+PromedioAno = my_data[, c("año", "prom_simp_x_ciclo")]
+PromedioAno$año = as.numeric(as.character(PromedioAno$año))
 row.names(PromedioAno) = my_data$ID
 nClustersPromAno = fviz_nbclust(PromedioAno, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("Número óptimo de clusters")
 clustersPromAno = kmeans(PromedioAno, 3, nstart = 30)
