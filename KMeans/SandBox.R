15a701f25d7db7e3b9138ecc6b592bb6d22915e7
Andoni Zamora <tonytaze096@gmail.com>
2019-11-26 23:06:31 -0600

    PDF plots y RData datasets de clusters

--- KMeans/SandBox.R
+++ KMeans/SandBox.R
@@ -11,25 +11,31 @@ library("gridExtra")
 library(ggplot2)
 
 setwd("C:/Users/Abraham/Desktop/Proyecto2Analisis") #comentar y agregar sus rutas
+setwd("D:/Documents/Universidad/Octavo ciclo/AN√ÅLISIS DE DATOS/Proyecto2/Proyecto2Analisis")
 
 # xlsx files
 my_data <- read_excel("ProjectData/ListadoPromedios.xlsx")
 set.seed(23544727)
 
 #Plot de promedio simple por anio
-ggplot() + geom_point(aes(x = my_data$aÒo, y =  (my_data$Promedio_Simple_Acumulado)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+pdf("KMeans/Plots/PromedioSimplePorAnio.pdf", width=22, height=12)
+ggplot() + geom_point(aes(x = my_data$a√±o, y =  (my_data$Promedio_Simple_Acumulado)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+dev.off()
 
 #Plot de promedio ponderado por anio
-ggplot() + geom_point(aes(x = my_data$aÒo, y =  (my_data$Promedio_Ponderado_Acumulado)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+pdf("KMeans/Plots/PromedioPondPorAnio.pdf", width=22, height=12)
+ggplot() + geom_point(aes(x = my_data$a√±o, y =  (my_data$Promedio_Ponderado_Acumulado)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+dev.off()
 
 #Box diagram de promedio ponderado por anio
-ggplot() + geom_boxplot(aes(x = my_data$aÒo, y =  (my_data$Promedio_Ponderado_Acumulado)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
-
+pdf("KMeans/Plots/PromedioPondPorAnioBox.pdf", width=20, height=12)
+ggplot() + geom_boxplot(aes(x = my_data$a√±o, y =  (my_data$Promedio_Ponderado_Acumulado)), data = my_data, alpha = 0.5) + ggtitle('Conjunto de Datos')
+dev.off()
 
 #Promedio simple x numero de cursos 
 PromedioSimple = my_data[,c("prom_simp_x_ciclo", "cursos_acumulados")]
 row.names(PromedioSimple) = my_data$ID
-nclustersPromSimp = fviz_nbclust(PromedioSimple, kmeans, method = "wss") + geom_vline(xintercept = 3 ,linetype = 2) + ggtitle("N˙mero Ûptimo de clusters")
+nclustersPromSimp = fviz_nbclust(PromedioSimple, kmeans, method = "wss") + geom_vline(xintercept = 3 ,linetype = 2) + ggtitle("N?mero ?ptimo de clusters")
 clustersPromSimp = kmeans(PromedioSimple, 3, nstart = 20)
 graficakPromSimp = fviz_cluster(clustersPromSimp, data = PromedioSimple) + ggtitle("Promedio Simple x Numero de cursos")
 save(PromedioSimple, file = "KMeans/Data/PromedioSimple.RData")
@@ -40,26 +46,24 @@ dev.off()
 
 PromedioPonderado = my_data[, c("Promedio_Ponderado_Acumulado", "cursos_acumulados")]
 row.names(PromedioPonderado) = my_data$ID
-nclustersPromPond = fviz_nbclust(PromedioPonderado, kmeans, method = "wss") + geom_vline(xintercept = 4, linetype = 2) + ggtitle("N˙mero Ûptimo de clusters")
+nclustersPromPond = fviz_nbclust(PromedioPonderado, kmeans, method = "wss") + geom_vline(xintercept = 4, linetype = 2) + ggtitle("N?mero ?ptimo de clusters")
 clustersPromPond = kmeans(PromedioPonderado, 4, nstart = 100)
-graficakPromPond = fviz_cluster(clustersPromPond, data = PromedioPonderado) + ggtitle("Promedio acomulado x N˙meroo de cursos")
+graficakPromPond = fviz_cluster(clustersPromPond, data = PromedioPonderado) + ggtitle("Promedio acomulado x N?meroo de cursos")
+save(PromedioPonderado, file = "KMeans/Data/PromedioPonderado.RData")
 pdf("KMeans/Plots/PromedioPond.pdf", width=30, height=50)
 grid.arrange(graficakPromPond, nclustersPromPond, nrow = 2)
 dev.off()
 
-PromedioAno = my_data[, c("aÒo", "prom_simp_x_ciclo")]
-PromedioAno$aÒo = as.numeric(as.character(PromedioAno$aÒo))
+PromedioAno = my_data[, c("a√±o", "prom_simp_x_ciclo")]
+PromedioAno$a√±o = as.numeric(as.character(PromedioAno$a√±o))
 row.names(PromedioAno) = my_data$ID
-nClustersPromAno = fviz_nbclust(PromedioAno, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("N˙mero Ûptimo de clusters")
+nClustersPromAno = fviz_nbclust(PromedioAno, kmeans, method = "wss") + geom_vline(xintercept = 3, linetype = 2) + ggtitle("N?mero ?ptimo de clusters")
 clustersPromAno = kmeans(PromedioAno, 3, nstart = 30)
-graficakPromAno = fviz_cluster(clustersPromAno, data = PromedioAno) + ggtitle("Promedio acomulado x AÒo (No relevante)")
+graficakPromAno = fviz_cluster(clustersPromAno, data = PromedioAno) + ggtitle("Promedio acomulado x A?o (No relevante)")
+save(PromedioAno, file = "KMeans/Data/PromedioAnio.RData")
 pdf("KMeans/Plots/PromedioAni.pdf", width=30, height=50)
 grid.arrange(graficakPromAno, nClustersPromAno, nrow = 2)
 dev.off()
-
-# Estudiante 119
-my_data <- read.csv("ProjectData/DataSets/119.csv")
-NotasNumericas = my_data[]
 
 
 
